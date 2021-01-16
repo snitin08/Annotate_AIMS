af025af5ad234e99c079fe39fc8a26564ab6b38c
mayhem-am <amakanksh@gmail.com>
2021-01-16 16:24:07 +0530

    pdf annotation tested

--- annotation/annotate/views.py
+++ annotation/annotate/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
-from annotation.settings import BASE_DIR
+from django.contrib import messages
+from annotation.settings import BASE_DIR,MEDIA_ROOT
+from pdf2image import convert_from_path
+import os
 
 # Create your views here.
 def annotate(request):
@@ -21,13 +24,28 @@ def upload_pdf(request):
         data = request.POST
         files = request.FILES
         file1 = request.FILES['file_name']
-        
         x = file1.read()
         f = open(BASE_DIR + '/media/current.pdf', 'wb')
         f.write(x)
         f.close()
-        
-        
+        pages = convert_from_path(
+            BASE_DIR+'/media/current.pdf', fmt="jpeg", size=(900,1200))
+        picture_path = MEDIA_ROOT
+        del_image_counter = 1
+        while True:
+            if os.path.exists(os.path.join(picture_path, "page-"+str(del_image_counter)+".jpeg")):
+                os.remove(os.path.join(picture_path, "page-" +str(del_image_counter)+".jpeg"))
+                del_image_counter+=1
+            else:
+                break
+        image_counter = 1
+        for page in pages:
+            # Save the image of the page in system
+            filename = "page-"+str(image_counter)+".jpeg"
+            page.save(picture_path+filename, 'JPEG')
+            # os.remove(filename)                              #comment if u want to operate on extracted images
+            image_counter = image_counter + 1
+        messages.info(request, 'Upload PDF successful')
         return HttpResponse("OK")
     else:
         return render(request,"annotate/upload_pdf.html",{})
--- annotation/static/js/index.js
+++ annotation/static/js/index.js
@@ -3,27 +3,29 @@
 window.onload = function() {
 
     canvas = document.getElementById('canvas');
-    console.log(canvas)
     ctx = canvas.getContext('2d');
+    prev = document.getElementById('pdf-prev');
+    next = document.getElementById('pdf-next');
     rect = {};
     drag = false;
     imageObj = null;
     coordinates = new Map();
+    annotations = new Map();
     id = 0;
+    var _CURRENT_PAGE = 1;
        
 
     function init() {
-        imageObj = new Image();
-        imageObj.onload = function () { ctx.drawImage(imageObj, 0, 0); };
-        imageObj.src = '/media/page_1.jpeg';
-        console.log(imageObj.src);
         canvas.addEventListener('mousedown', mouseDown, false);
         canvas.addEventListener('mouseup', mouseUp, false);
         canvas.addEventListener('mousemove', mouseMove, false);
+        prev.addEventListener('click',getprev,false);
+        next.addEventListener('click', getnex, false);
+        _CURRENT_PAGE = 1;
+        showPage(1);
     }
 
     function mouseDown(e) {
-        console.log(e.pageX);
         rect.startX = e.pageX - this.offsetLeft;
         rect.startY = e.pageY - this.offsetTop;
         drag = true;
@@ -41,11 +43,9 @@ window.onload = function() {
                     <td><input type='text' class='form-control' style='width:250px;' form='coordinate_form' name='label' class='form-control' list='labelOptions'></td>\
                     <td><button class='cross' onclick='removeAnnotation("+id+")'> X </button></td>\
                 </tr>"
-            )
-        coordinates.set(id.toString(),[rect.startX,rect.startY,rect.w,rect.h]);
+            );
+        coordinates.set(id.toString(),[rect.startX,rect.startY,rect.w,rect.h,""]);
         id++;
-        console.log(coordinates);
-        
     }
 
     function mouseMove(e) {
@@ -89,18 +89,16 @@ function ShowRect(id)
     if(!coordinate)
         return;
     ctx.lineWidth = 5;
-    ctx.strokeStyle = 'red';
+    ctx.strokeStyle = 'green';
     ctx.strokeRect(coordinate[0],coordinate[1],coordinate[2],coordinate[3]);
     ctx.lineWidth = 1;
 }
 
 function removeAnnotation(id)
 {
-    
     id = id.toString();
-
     coordinates.delete(id);
-    $("tr[id="+id+"]").remove()
+    $("tr[id="+id+"]").remove();
     ctx.clearRect(0, 0, 900, 1200);                
     ctx.drawImage(imageObj, 0, 0);   
     
@@ -113,3 +111,104 @@ function removeAnnotation(id)
     }
     
 }
+
+function UrlExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status != 404;
+}
+
+function showPage(page_no) {
+    _CURRENT_PAGE = page_no;
+
+    // Disable Prev & Next buttons while page is being loaded
+    $("#pdf-next, #pdf-prev").attr('disabled', 'disabled');
+
+    // While page is being rendered hide the canvas and show a loading message
+    
+
+    // Update current page in HTML
+    $("#pdf-current-page").text(page_no);
+    
+    // Fetch the page
+    
+    imageObj = new Image();
+    imageObj.src = '/media/page-' + _CURRENT_PAGE.toString() + '.jpeg';
+    console.log(UrlExists(imageObj.src));
+    if(UrlExists(imageObj.src)==false){
+        --_CURRENT_PAGE;
+        showPage(_CURRENT_PAGE);
+        $("#pdf-current-page").text(_CURRENT_PAGE);
+        $("#pdf-next, #pdf-prev").removeAttr('disabled');
+    }
+    else{
+        id=0;
+        imageObj.onload = function () {
+        canvas.width = 900;
+        canvas.height = 1200;
+        ctx.clearRect(0, 0, 900, 1200);
+        ctx.drawImage(imageObj, 0, 0);
+        pagecoors = annotations['Page' + _CURRENT_PAGE.toString()];
+        if(typeof pagecoors !== 'undefined'){
+            for (let i of pagecoors.keys()) {
+                coordinate = pagecoors.get(i.toString());
+                $("#annotation_table_body").append(
+                    "<tr id='" + i + "' onclick='ShowRect(" + i + ");'>\
+                        <td><input type='text' class='form-control' readonly name='startX' form='coordinate_form' value='"+ coordinate[0] + "'></td>\
+                        <td><input type='text' class='form-control' readonly name='startY' form='coordinate_form' value='"+ coordinate[1]+ "'></td>\
+                        <td><input type='text' class='form-control' readonly name='w' form='coordinate_form' value='"+ coordinate[2]+ "'></td>\
+                        <td><input type='text' class='form-control' readonly name='h' form='coordinate_form' value='"+ coordinate[3]+ "'></td>\
+                        <td><input type='text' class='form-control' style='width:250px;' form='coordinate_form' name='label' class='form-control' list='labelOptions' value='"+ coordinate[4]+"'></td>\
+                        <td><button class='cross' onclick='removeAnnotation("+ i + ")'> X </button></td>\
+                    </tr>"
+                )
+                coordinates.set(i.toString(), [coordinate[0], coordinate[1], coordinate[2], coordinate[3],coordinate[4]]);
+                ctx.strokeStyle = 'red';
+                ctx.strokeRect(coordinate[0], coordinate[1], coordinate[2], coordinate[3]);
+                id = Math.max(id,i)+1;
+            }
+        }
+        };
+        $("#pdf-next, #pdf-prev").removeAttr('disabled');
+    }
+}
+
+function getprev() {
+    annotations['Page' + _CURRENT_PAGE.toString()] = new Map();
+    curpagecoors = annotations['Page' + _CURRENT_PAGE.toString()];
+    if (_CURRENT_PAGE != 1){
+        for (let i of coordinates.keys()) {
+            coordinate = coordinates.get(i.toString());
+            var $row = $("tr[id=" + i + "]");
+            $tds = $row.find("td:nth-child(5) input[type='text']");
+            coordinate[4] = $tds.val();
+            $row.remove();
+            curpagecoors.set(i.toString(), [coordinate[0], coordinate[1], coordinate[2], coordinate[3], coordinate[4]]);
+        }
+        for (let i of coordinates.keys()) {
+            id = i.toString();
+            coordinates.delete(id);
+        }
+        showPage(--_CURRENT_PAGE);
+    }
+};
+
+// Next page of the PDF
+function getnex() {
+    annotations['Page' + _CURRENT_PAGE.toString()] = new Map();
+    curpagecoors = annotations['Page' + _CURRENT_PAGE.toString()];
+    for (let i of coordinates.keys()) {
+        coordinate = coordinates.get(i.toString());
+        var $row = $("tr[id=" + i + "]");
+        $tds = $row.find("td:nth-child(5) input[type='text']");
+        coordinate[4] = $tds.val(); 
+        $row.remove();
+        curpagecoors.set(i.toString(), [coordinate[0], coordinate[1], coordinate[2], coordinate[3], coordinate[4]]);
+    }
+    for (let i of coordinates.keys()) {
+        id = i.toString();
+        coordinates.delete(id);
+    }
+    showPage(++_CURRENT_PAGE);
+};
