2a9835c9a2813245a59122c6d2d7e4dcdc0c4faf
snitin08 <nitinpandit.2000@gmail.com>
2021-03-02 13:51:53 +0530

    styling

--- annotation/annotation/urls.py
+++ annotation/annotation/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from annotate.views import annotate
 
 urlpatterns = [
+    path('',annotate,name='index'),
     path('annotate/',include('annotate.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
